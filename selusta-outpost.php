38813a4c9c698900e2da4131bb2e69a0df11a192
Juho Räsänen <8088372+RJuho@users.noreply.github.com>
2018-10-18 20:45:53 +0300

    WP file

---
+++ selusta-outpost.php
@@ -0,0 +1,14 @@
+<?php
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+/*
+Plugin Name:  Selusta Outpost
+Plugin URI:   https://selusta.com/outpost
+Description:  Selusta Outpost Wordpress integration plugin, security first
+Version:      20181018
+Author:       Selusta
+Author URI:   https://selusta.com/
+License:      
+License URI:  
+Text Domain:  
+Domain Path:  
+*/
