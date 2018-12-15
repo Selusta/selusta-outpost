712b70f2847afdb2a43a3e8f134fb4e44b1b4c92
Juho Räsänen <8088372+RJuho@users.noreply.github.com>
2018-12-15 14:24:20 +0200

    testi'

---
+++ hooks.php
@@ -0,0 +1,15 @@
+<?php 
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+function selustaActivate() {
+
+}
+
+function selustaDeInstall() {
+
+}
+
+function selustaUnistall() {
+
+}
+ ?>
