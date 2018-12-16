6509c010cfdf2e4c91118ff1efaf1eee4a239163
Juho Räsänen <juho.rasanen@setuppi.fi>
2018-12-16 20:54:28 +0200

    New function added
    createFileTree( path , hash protocol )
    return array file tree
    hash protocols available md5 and sha1

---
+++ includes/includes.php
@@ -0,0 +1,7 @@
+<?php
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+require_once( 'main.php' ); // menu items etc.
+require_once( 'actions.php' ); // activate, deactivate, unistall
+require_once( 'scan.php' ); // scanning functions
+?>
